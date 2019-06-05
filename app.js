d92771b5313220e292e498a6578595809fad4688
Robert Kramer <robert.n.kramer@gmail.com>
2019-06-04 21:19:51 -0700

    initial shapes, movement, spacebar start/stop

--- app.js
+++ app.js
@@ -3,20 +3,72 @@ window.onload = () => {
 
   const canvas = document.getElementById("gameCanvas");
   const canvasContext = canvas.getContext("2d");
+  
 
-  let xLoc = 100;
-  let gameSpeed = 10;
+  const board = {
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 600,
+    color: "#000"
+  };
 
-  setInterval(() => {
-    // draw the game board
-    drawRect(0, 0, canvas.width, canvas.height, "black");
-    
-    drawRect(xLoc, 100, 10, 10, "red");
-    xLoc += 5;
-  }, 10);
+  const apple = {
+    x: 395,
+    y: 295,
+    width: 10,
+    height: 10,
+    color: "#ff0000",
+    xVel: 5,
+    yVel: 5
+  };
 
+  const game = {
+    on: false,
+    speed: 50,
+    action: null
+  };
+
+  
   function drawRect(leftX, topY, width, height, objColor) {
     canvasContext.fillStyle = objColor;
     canvasContext.fillRect(leftX, topY, width, height);
   }
+  
+  function init() {
+    drawRect(board.x, board.y, board.width, board.height, board.color);
+    drawRect(apple.x, apple.y, apple.width, apple.height, apple.color);
+  }
+  
+  document.addEventListener("keydown", e => {
+    if (event.which === 32) {
+      game.on = !game.on;
+    }
+    if (game.on) {
+      game.action = setInterval(() => {
+        // draw the game board
+        drawRect(board.x, board.y, board.width, board.height, board.color);
+
+        snakeMove();
+      }, game.speed);
+    } else {
+      clearInterval(game.action);
+    }
+  });
+
+  function snakeMove() {
+    if (apple.x >= board.width - 10 || apple.x <= 0) {
+      apple.xVel = -apple.xVel;
+    }
+
+    if (apple.y >= board.height - 10 || apple.y <= 0) {
+      apple.yVel = -apple.yVel;
+    }
+
+    apple.y += apple.yVel;
+    apple.x += apple.xVel;
+    drawRect(apple.x, apple.y, apple.width, apple.height, apple.color);
+  }
+
+  init();
 };
