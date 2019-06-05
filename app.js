64c4f193bac002dda134b8b64473eb37aa0fac55
Robert Kramer <robert.n.kramer@gmail.com>
2019-06-04 17:51:05 -0700

    objects added

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('connected')
+window.onload = () => {
+  console.log("Window loaded");
+
+  const canvas = document.getElementById("gameCanvas");
+  const canvasContext = canvas.getContext("2d");
+
+  let xLoc = 100;
+  let gameSpeed = 10;
+
+  setInterval(() => {
+    // draw the game board
+    drawRect(0, 0, canvas.width, canvas.height, "black");
+    
+    drawRect(xLoc, 100, 10, 10, "red");
+    xLoc += 5;
+  }, 10);
+
+  function drawRect(leftX, topY, width, height, objColor) {
+    canvasContext.fillStyle = objColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+  }
+};
